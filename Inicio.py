0606d4b86812d965e0efe572b79d1aa3199843f6
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-05-19 00:36:17 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from PIL import Image
+from PIL import Image2
+from PIL import Image3
 
 st.title("Control de luces y puerta en casa")
 image = Image.open("Lights.jpg")
+
+st.header("¿Cómo puedo utilizar el asistente de control del hogar?")
+st.write("En la página de Luces podrás cambiar el color de las luces a través de dos gestos:")
+
+
 
