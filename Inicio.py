7fac07cee6ae20d9b18a4d4b8b9f5ad0677194dc
mlopezo2 <160878713+mlopezo2@users.noreply.github.com>
2024-05-19 00:56:23 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 st.title("Control de luces y puerta en casa")
 image = Image.open("Lights.jpg")
+st.image(image, caption="Luces de interior")
 
 st.header("¿Cómo puedo utilizar el asistente de control del hogar?")
 st.write("En la página de Luces podrás cambiar el color de las luces a través de dos gestos:")
